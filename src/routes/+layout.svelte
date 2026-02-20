2d2c010b27e95daf8ae1f0bbe39ebce4102f6e19
Daniel McFadyen <shadyshadyshades@hotmail.com>
2026-02-21 09:53:13 +1100

    changing tabs on one page to multiple pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+    import { CalendarIcon, CircleUserIcon, MenuIcon, SearchIcon } from 'lucide-svelte';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 </script>
 <style>
 	    @font-face{
@@ -23,4 +25,24 @@
 </style>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
+<AppBar class="bg-primary-300 text-white">
+	<AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+		<AppBar.Lead>
+			<a href="/"><button type="button" class="btn preset-outlined-primary-500 hover:bg-white hover:text-primary-900">Fed 25</button></a>
+			<a href="/demographics"><button type="button" class="btn preset-outlined-primary-500 hover:text-primary-900">Demographics</button></a>
+			<a href="/simulation"><button type="button" class="btn preset-outlined-primary-500 hover:text-primary-900">FP Simulations</button></a>
+		</AppBar.Lead>
+		<AppBar.Headline>
+						
+			
+		</AppBar.Headline>
+		<AppBar.Trail>
+			<button type="button" class="btn-icon hover:preset-tonal"><SearchIcon class="size-6" /></button>
+			<button type="button" class="btn-icon hover:preset-tonal"><CalendarIcon class="size-6" /></button>
+			<button type="button" class="btn-icon hover:preset-tonal"><CircleUserIcon class="size-6" /></button>
+		</AppBar.Trail>
+	</AppBar.Toolbar>
+</AppBar>
 {@render children()}
